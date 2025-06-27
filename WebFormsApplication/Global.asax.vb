af2c0fbfe744d422e7220e6ce70520e4710c9a05
Student2 <Student2@PAM-H508LS3>
2025-06-27 15:01:52 +0100

    Add project files.

---
+++ WebFormsApplication/Global.asax.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Web.Optimization
+
+Public Class Global_asax
+    Inherits HttpApplication
+
+    Sub Application_Start(sender As Object, e As EventArgs)
+        ' Fires when the application is started
+        RouteConfig.RegisterRoutes(RouteTable.Routes)
+        BundleConfig.RegisterBundles(BundleTable.Bundles)
+
+        If Not Roles.RoleExists("Admin") Then
+            Roles.CreateRole("Admin")
+        End If
+
+        ' Create user if it doesn't exist
+        If Membership.GetUser("admin") Is Nothing Then
+            Membership.CreateUser("admin", "Password123!", "admin@example.com")
+            Roles.AddUserToRole("admin", "Admin")
+        End If
+    End Sub
+End Class
---
+++ WebFormsApplication/Login.aspx.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Security.Claims
+Imports Microsoft.AspNetCore.DataProtection
+Imports Microsoft.Owin.Security
+Imports Microsoft.Owin.Security.Cookies
+Public Class Login
+    Inherits System.Web.UI.Page
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Request.QueryString("Logout") IsNot Nothing Then
+            Logout()
+        End If
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        If tbUserName.Text.Length > 0 And tbPassword.Text.Length > 0 Then 'perform other validation logic as needed
+            'FormsAuthentication.Authenticate(tbUserName.Text, tbPassword.Text) has been deprecated
+            If Membership.ValidateUser(tbUserName.Text, tbPassword.Text) Then
+                Logon(tbUserName.Text)
+            Else
+                lblMessage.Text = "Credentials were not valid."
+            End If
+        Else
+            lblMessage.Text = "You must enter both a user name and password. "
+        End If
+    End Sub
+    Private Sub Logon(UserName As String)
+        Dim lh As New LoginHelper
+        Dim persistCookie As Boolean = True
+
+        FormsAuthentication.SetAuthCookie(UserName, persistCookie)
+        'lh.LoginForms(UserName, persistCookie, Context)
+        lh.LoginClaims(UserName, persistCookie, Context)
+
+        Dim strRedirect As String = Request("ReturnURL")
+        If strRedirect = "" Then
+            strRedirect = "/"
+        End If
+
+        'or:
+        'strRedirect = FormsAuthentication.GetRedirectUrl(UserName, persistCookie)
+
+        Response.Redirect(strRedirect, True)
+
+    End Sub
+
+    Private Sub Logout()
+
+        Dim lh As New LoginHelper
+        lh.Logout(Context)
+        Response.Redirect("~/")
+    End Sub
+
+End Class
